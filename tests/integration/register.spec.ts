6d20b3ff204c556fcbe8475686cd0d745b16c229
thallesyam <thallesyam@gmail.com>
2022-12-03 22:24:17 -0300

    test: ensure only one same email in register

--- tests/integration/register.spec.ts
+++ tests/integration/register.spec.ts
@@ -1,18 +1,35 @@
 import { UserRegister } from '@/application/usecases/user-register'
 import { UsersRepositoryMemory } from '@/infra/repositories/memory'
 import { GenerateCryptoId } from '@/infra/gateways'
+import { UserAlreadyRegister } from '@/domain/errors'
 
 test('Deve criar um usuário e retornar a quantidade correta no repositório', async () => {
-  const usersRepository = new UsersRepositoryMemory()
   const sut = {
     name: 'Thalles Ian',
     email: 'thallesyam@gmail.com',
     image: 'fake-image',
     password: '123'
   }
+  const usersRepository = new UsersRepositoryMemory()
   const generateIdGateway = new GenerateCryptoId()
   const registerUser = new UserRegister(usersRepository, generateIdGateway)
-  registerUser.execute(sut)
+  await registerUser.execute(sut)
   const users = await usersRepository.findAll()
   expect(users.length).toBe(1)
+})
+
+test('Deve tentar criar um usuário com um email já existente', async () => {
+  const sut = {
+    name: 'Thalles Ian',
+    email: 'thallesyam@gmail.com',
+    image: 'fake-image',
+    password: '123'
+  }
+  const usersRepository = new UsersRepositoryMemory()
+  const generateIdGateway = new GenerateCryptoId()
+  const registerUser = new UserRegister(usersRepository, generateIdGateway)
+  await registerUser.execute(sut)
+  expect(async () => await registerUser.execute(sut)).rejects.toThrowError(
+    UserAlreadyRegister
+  )
 })

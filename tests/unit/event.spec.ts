92fb4c2eddb91e38ee5ce4e86c2e5e538468f29d
thallesyam <thallesyam@gmail.com>
2022-12-06 08:38:15 -0300

    refactor: remove user instance to event unit test

--- tests/unit/event.spec.ts
+++ tests/unit/event.spec.ts
@@ -3,16 +3,9 @@ import { Event } from '@/domain/entities'
 import { InvalidEvent } from '@/domain/errors'
 
 test('Deve criar um evento com os dados corretos', () => {
-  const user = new User(
-    '1',
-    'Thalles Ian',
-    'thallesyam@gmail.com',
-    'fake-image',
-    '123'
-  )
   const sut = new Event(
     '1',
-    user.userId,
+    '1',
     'Aula sobre typescript',
     'Aula voltada para enterder o básico da sintaxe typescript',
     new Date('2022-12-06T12:00:00'),
@@ -28,16 +21,9 @@ test('Deve criar um evento com os dados corretos', () => {
 })
 
 test('Deve criar um evento na modalidade remota', () => {
-  const user = new User(
-    '1',
-    'Thalles Ian',
-    'thallesyam@gmail.com',
-    'fake-image',
-    '123'
-  )
   const sut = new Event(
     '1',
-    user.userId,
+    '1',
     'Aula sobre typescript',
     'Aula voltada para enterder o básico da sintaxe typescript',
     new Date('2022-12-06T12:00:00'),
@@ -49,16 +35,9 @@ test('Deve criar um evento na modalidade remota', () => {
 })
 
 test('Deve criar um evento com um usuário inscrito', () => {
-  const user = new User(
-    '1',
-    'Thalles Ian',
-    'thallesyam@gmail.com',
-    'fake-image',
-    '123'
-  )
   const sut = new Event(
     '1',
-    user.userId,
+    '1',
     'Aula sobre typescript',
     'Aula voltada para enterder o básico da sintaxe typescript',
     new Date('2022-12-06T12:00:00'),
@@ -72,19 +51,11 @@ test('Deve criar um evento com um usuário inscrito', () => {
 })
 
 test('Deve tentar criar um evento com os dados incorretos', () => {
-  const user = new User(
-    '1',
-    'Thalles Ian',
-    'thallesyam@gmail.com',
-    'fake-image',
-    '123'
-  )
-
   expect(
     () =>
       new Event(
         '1',
-        user.userId,
+        '1',
         '',
         'Aula voltada para enterder o básico da sintaxe typescript',
         new Date('2022-12-06T12:00:00'),

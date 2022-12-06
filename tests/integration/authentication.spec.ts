3324c9951c130809e415a794b6b4ff3218dddcf0
thallesyam <thallesyam@gmail.com>
2022-12-05 21:39:29 -0300

    test: implements jsonwebtoken return and create fail auth

--- tests/integration/authentication.spec.ts
+++ tests/integration/authentication.spec.ts
@@ -1,8 +1,10 @@
 import { UserAuthentication } from '@/application/usecases/user-authentication'
 import { User } from '@/domain/entities'
+import { UserNotFound } from '@/domain/errors/user-not-found'
+import { GenerateToken } from '@/infra/gateways/generate-token'
 import { UsersRepositoryMemory } from '@/infra/repositories/memory'
 
-test('Deve conseguir se autenticar utilizando email e senha', async () => {
+test('Deve se autenticar utilizando email e senha', async () => {
   const user = new User(
     '1',
     'Victoria',
@@ -11,12 +13,35 @@ test('Deve conseguir se autenticar utilizando email e senha', async () => {
     '123'
   )
   const usersRepository = new UsersRepositoryMemory()
+  const generateTokenGateway = new GenerateToken()
   await usersRepository.save(user)
-  const sut = new UserAuthentication(usersRepository)
+  const sut = new UserAuthentication(usersRepository, generateTokenGateway)
   const input = {
     email: user.email,
     password: user.password
   }
   await sut.execute(input)
   expect(sut).toBeTruthy()
+})
+
+test('Deve tentar se autenticar utilizando email e senha inválidos', async () => {
+  const user = new User(
+    '1',
+    'Victoria',
+    'vsamoraa@gmail.com',
+    'fake-image',
+    '123'
+  )
+  const usersRepository = new UsersRepositoryMemory()
+  const generateTokenGateway = new GenerateToken()
+  await usersRepository.save(user)
+  const sut = new UserAuthentication(usersRepository, generateTokenGateway)
+  const input = {
+    email: 'thallesyam@gmail.com',
+    password: user.password
+  }
+
+  expect(async () => await sut.execute(input)).rejects.toThrowError(
+    UserNotFound
+  )
 })

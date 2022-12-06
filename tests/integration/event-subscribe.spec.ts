5b233634ebfd50e76624c7293cb717d3b1c15c23
thallesyam <thallesyam@gmail.com>
2022-12-06 17:18:05 -0300

    test: ensure throw error if user or event id not exists

--- tests/integration/event-subscribe.spec.ts
+++ tests/integration/event-subscribe.spec.ts
@@ -1,5 +1,6 @@
 import { EventSubscribe } from '@/application/usecases/event-subscribe'
 import { Event, User } from '@/domain/entities'
+import { EventIdNotFound, UserIdNotFound } from '@/domain/errors'
 import { GenerateCryptoId } from '@/infra/gateways'
 import {
   UsersRepositoryMemory,
@@ -57,17 +58,102 @@ test('Deve realizar uma inscrição com um usuário e evento válido', async () 
   expect(enrollments.length).toBe(1)
 })
 
-// test.todo(
-//   'Deve tentar realizar uma inscrição com um usuário e evento inválido',
-//   () => {}
-// )
+test('Deve tentar realizar uma inscrição com um usuário inválido', async () => {
+  const date = new Date()
+  const owner = new User(
+    '1',
+    'Thalles Ian',
+    'thallesyam@gmail.com',
+    'fake-image',
+    '123'
+  )
+  const subscriber = new User(
+    '2',
+    'Akin',
+    'akin@gmail.com',
+    'fake-image',
+    '123'
+  )
+  const event = new Event(
+    '1',
+    owner.userId,
+    'Aula sobre typescript',
+    'Aula voltada para enterder o básico da sintaxe typescript',
+    new Date(date.setDate(date.getDate() + 1)),
+    '11932245266',
+    100,
+    'onSite',
+    'Rua Francisco da cunha'
+  )
+  const generateIdGateway = new GenerateCryptoId()
+  const eventRepository = new EventRepositoryMemory()
+  const usersRepository = new UsersRepositoryMemory()
+  const enrollmentRepository = new EnrollmentRepositoryMemory()
+  await usersRepository.save(owner)
+  await usersRepository.save(subscriber)
+  await eventRepository.save(event)
+  const sut = new EventSubscribe(
+    eventRepository,
+    usersRepository,
+    enrollmentRepository,
+    generateIdGateway
+  )
 
-// test.todo(
-//   'Deve tentar realizar uma inscrição com um usuário inexistente',
-//   () => {}
-// )
+  const input = {
+    subscriberId: '3',
+    eventId: event.eventId
+  }
+  expect(async () => await sut.execute(input)).rejects.toThrowError(
+    UserIdNotFound
+  )
+})
 
-// test.todo(
-//   'Deve tentar realizar uma inscrição com um evento inexistente',
-//   () => {}
-// )
+test('Deve tentar realizar uma inscrição com um evento inexistente', async () => {
+  const date = new Date()
+  const owner = new User(
+    '1',
+    'Thalles Ian',
+    'thallesyam@gmail.com',
+    'fake-image',
+    '123'
+  )
+  const subscriber = new User(
+    '2',
+    'Akin',
+    'akin@gmail.com',
+    'fake-image',
+    '123'
+  )
+  const event = new Event(
+    '1',
+    owner.userId,
+    'Aula sobre typescript',
+    'Aula voltada para enterder o básico da sintaxe typescript',
+    new Date(date.setDate(date.getDate() + 1)),
+    '11932245266',
+    100,
+    'onSite',
+    'Rua Francisco da cunha'
+  )
+  const generateIdGateway = new GenerateCryptoId()
+  const eventRepository = new EventRepositoryMemory()
+  const usersRepository = new UsersRepositoryMemory()
+  const enrollmentRepository = new EnrollmentRepositoryMemory()
+  await usersRepository.save(owner)
+  await usersRepository.save(subscriber)
+  await eventRepository.save(event)
+  const sut = new EventSubscribe(
+    eventRepository,
+    usersRepository,
+    enrollmentRepository,
+    generateIdGateway
+  )
+
+  const input = {
+    subscriberId: subscriber.userId,
+    eventId: '3'
+  }
+  expect(async () => await sut.execute(input)).rejects.toThrowError(
+    EventIdNotFound
+  )
+})
